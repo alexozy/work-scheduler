a3ff9d761df760746280eefc15730d2e153ed1d8
Ozanne Alexander <alexanoz@outlook.com>
2022-11-26 22:40:10 -0500

    added local storage setItem

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,4 +1,31 @@
-//
 // display current time?
+var dateDisplayEl = document.querySelector("#date-Display")
+//makes the clock tick
+setInterval (function () {
+    dateDisplayEl.textContent = (moment().format('ddd, MMM Do YYYY, h:mm:ss a'));
+}, 1000)
 
-// saveBtn Local Storage?
+var currentTime = moment().hour();
+
+
+
+
+
+// saveBtn Local Storage? 
+
+// add onClick & annon function to localStorage.setItem (key,value). I need the key and value defined
+// $(".saveBtn").on("click", function () {
+//     var key = $(this).parent().attr("id").split("-")[1];
+//   var value = $(this).parent().find(".description").val();
+//   localStorage.setItem(key,value)
+// });
+
+// .each controls content inside () ; we are in charge of content in {}
+$(".saveBtn").each(function (i, btn) {
+  $(btn).on("click",function (){
+    // we want to grab the info in textarea and not the actual button
+    var value = $(this).siblings(".description").val();
+    var key = $(this).parent().attr("id");
+    localStorage.setItem(key,value)
+  })
+});
